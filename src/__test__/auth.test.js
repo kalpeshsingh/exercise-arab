940dc24eae91cef4446e88083c84c39bb62647f0
Kalpesh Singh <kalpesin@amazon.com>
2020-08-02 14:19:28 +0530

    fix(auth): restore date implementation

--- src/__test__/auth.test.js
+++ src/__test__/auth.test.js
@@ -7,8 +7,11 @@ test('should return true for authenticated function', () => {
     Storage.prototype.getItem = jest.fn(() => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNTE2MjQwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.ervjmmR5MAjz-ZJpEO8nhQpptXclhoJJn1-iDMw6ULA');
 
     const mockDate = new Date(1516239020);
-    jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
+    const mockDateImplementation = jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
 
     expect(mockAuthenticated()).toBe(true);
+
+    mockDateImplementation.mockRestore();
+
 });
 
