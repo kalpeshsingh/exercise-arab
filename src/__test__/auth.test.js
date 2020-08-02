067632ab26e35e01cadfd88578fa2ba16fffa30f
Kalpesh Singh <kalpesin@amazon.com>
2020-08-02 13:48:10 +0530

    refactor(auth): simplify 'authenticated' test

--- src/__test__/auth.test.js
+++ src/__test__/auth.test.js
@@ -1,18 +1,10 @@
 import {authenticated as mockAuthenticated} from '../auth';
-import {parseJwt} from '../utils';
-
-jest.mock('../utils');
 
 test('should return true for authenticated function', () => {
 
     jest.spyOn(Storage.prototype, 'getItem');
-    Storage.prototype.getItem = jest.fn(() => 'abc');
 
-    parseJwt.mockImplementation(() => {
-        return {
-            exp: 1516240
-        }
-    });
+    Storage.prototype.getItem = jest.fn(() => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxNTE2MjQwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.ervjmmR5MAjz-ZJpEO8nhQpptXclhoJJn1-iDMw6ULA');
 
     const mockDate = new Date(1516239020);
     jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
